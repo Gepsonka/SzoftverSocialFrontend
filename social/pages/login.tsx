44a717fd37f7bcb7b0ea1b39f80d6d251a211d78
Molnár Botond <45335158+Gepsonka@users.noreply.github.com>
2022-10-06 00:33:05 +0200

    register and login view are ready
    Functionality wil be added after implemented on the backend

---
+++ social/pages/login.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Card } from 'primereact/card';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+import { Button } from 'primereact/button';
+import { loadavg } from 'os';
+
+
+const Login: NextPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [usernameIsEmpty, setUsernameIsEmpty] = useState(false);
+  const [passwordIsEmpty, setPasswordIsEmpty] = useState(false);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const login = () => {
+    setIsLoading(true);
+    if (username === '' && password === ''){
+      setUsernameIsEmpty(true);
+      setPasswordIsEmpty(true);
+      setIsLoading(false);
+      return;
+    } else if (username === '') {
+      setUsernameIsEmpty(true);
+      setIsLoading(false);
+      return;
+    } else if (password === '') {
+      setPasswordIsEmpty(true);
+      setIsLoading(false);
+      return;
+    }
+    // TODO: implement login with jwt
+
+    setIsLoading(false);
+  }
+
+  return (
+    <div className='grid justify-content-center h-screen align-content-center auth-bg'>
+      <Card className='col-4 md:col-4 sm:col-10 text-center'>  
+        <h2 className='mb-5 mt-1' >Login</h2>
+        <div className="field mb-5">
+          <span className="p-float-label p-fluid">
+            <InputText className={`${usernameIsEmpty ? 'p-invalid' : ''}`} id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <label htmlFor="username">Username</label>
+          </span>
+        </div>
+        <div className="field md-5">
+          <span className="p-float-label p-fluid mb-2">
+            <Password id='password' className={`${passwordIsEmpty ? 'p-invalid' : ''}`} value={password} onChange={(e) => setPassword(e.target.value)} toggleMask  feedback={false} />
+            <label htmlFor="password">Password</label>
+          </span>
+        </div>
+        <Button label='Login' onClick={() => {login()}} loading={isLoading} />
+        <p style={{textAlign: 'left'}}>Or if don't have account <Button label="register here" className="p-button-link inline p-0" />.</p>
+      </Card>
+    </div>
+    
+  )
+}
+
+export default Login
---
+++ social/pages/register.tsx
@@ -0,0 +1,157 @@
+import React, { useState } from 'react';
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Card } from 'primereact/card';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+import { Button } from 'primereact/button';
+import { Calendar } from 'primereact/calendar';
+
+const Register: NextPage = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordAgain, setPasswordAgain] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [dateOfBirth, setDateOfBirth] = useState(undefined);
+
+    const [usernameIsEmpty, setUsernameIsEmpty] = useState(false);
+    const [emailIsEmpty, setEmailIsEmpty] = useState(false);
+    const [passwordIsEmpty, setPasswordIsEmpty] = useState(false);
+    const [passwordAgainIsEmpty, setPasswordAgainIsEmpty] = useState(false);
+    const [firstNameIsEmpty, setFirstNameIsEmpty] = useState(false);
+    const [lastNameIsEmpty, setLastNameIsEmpty] = useState(false);
+    const [dateOfBirthIsEmpty, setDateOfBirthIsEmpty] = useState(false);
+
+    const [isRegisterLoading, setIsRegisterLoading] = useState(false);
+    const [usernameIsTaken, setUsernameIsTaken] = useState(false);
+    const [passowrdsAreMatching, setPasswordsAreMatching] = useState(false);
+    const [emailIsTaken, setEmailIsTaken] = useState(false);
+
+    const [isUsernameTakenLoading, setIsUsernameTakenLoading] = useState(false);
+    const [isEmailTakenLoading, setIsEmailTakenLoading] = useState(false);
+
+
+    const register = async () => {
+        setIsRegisterLoading(true);
+        let anyEmpty = false;
+        
+        if (username === ''){
+            setUsernameIsEmpty(true);
+            anyEmpty = true;
+        }
+
+        if (email === ''){
+            setEmailIsEmpty(true);
+            anyEmpty = true;
+        }
+
+        if (password === ''){ 
+            setPasswordIsEmpty(true);
+            anyEmpty = true;
+        }
+
+        if (passwordAgain === ''){
+            setPasswordAgainIsEmpty(true);
+            anyEmpty = true;
+        }
+
+        if (firstName === ''){
+            setFirstNameIsEmpty(true);
+            anyEmpty = true;
+        }
+
+        if (lastName === ''){
+            setLastNameIsEmpty(true);
+            anyEmpty = true;
+        }
+
+        if (dateOfBirth === undefined){
+            setDateOfBirthIsEmpty(true);
+            anyEmpty = true;
+        }
+
+        if (anyEmpty){
+            setIsRegisterLoading(false);
+            return;
+        }
+
+        setIsRegisterLoading(false);
+        // TODO: request implementation
+    }
+
+    const checkUsernameIsTaken = async () => {
+        // TODO: implementation
+    }
+
+    const checkEmailIsTaken = async () => {
+        // TODO: implementation
+    }
+
+    return (
+        <div className='grid justify-content-center h-screen align-content-center auth-bg'>
+            <Card className='col-4 md:col-4 sm:col-10 text-center'>  
+                <h2 className='mb-5 mt-1' >Create your account</h2>
+                <div className='p-fluid'>
+                <div className="field mb-5">
+                    <span className={`p-float-label p-input-icon-left`}>
+                        <i className="pi pi-dot" />
+                        <InputText className={`${firstNameIsEmpty ? 'p-invalid' : ''}`} id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                        <label htmlFor="firstName">First Name*</label>
+                    </span>
+                </div>
+                <div className="field mb-5">
+                    <span className={`p-float-label p-input-icon-left`}>
+                        <i className="pi pi-dot" />
+                        <InputText className={`${lastNameIsEmpty ? 'p-invalid' : ''}`} id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                        <label htmlFor="lastName">Last Name*</label>
+                    </span>
+                </div>
+                <div className="field mb-5">
+                    <span className="p-float-label">
+                        <Calendar id="dateOfBirth" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.value)} />
+                        <label htmlFor="dateOfBirth">Date of Birth*</label>
+                    </span>
+                </div>
+                <div className="field mb-5">
+                    <span className={`p-float-label p-input-icon-left ${isUsernameTakenLoading ? 'p-input-icon-right' : ''}`}>
+                        {isUsernameTakenLoading && <i className="pi pi-spin pi-spinner" />}
+                        <i className="pi pi-search" />
+                        <InputText className={` ${usernameIsEmpty || usernameIsTaken ? 'p-invalid' : ''}`} id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <label htmlFor="username">Username*</label>
+                    </span>
+                </div>
+                <div className="field mb-5">
+                    <span className={`p-float-label p-input-icon-left ${isEmailTakenLoading ? 'p-input-icon-right' : ''}`}>
+                        {isEmailTakenLoading && <i className="pi pi-spin pi-spinner" />}
+                        <i className="pi pi-dot" />
+                        <InputText className={`${emailIsEmpty || emailIsTaken ? 'p-invalid' : ''}`} id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <label htmlFor="emial">Email*</label>
+                    </span>
+                </div>
+
+                <div className="field md-5">
+                    <span className="p-float-label p-input-icon-left mb-2">
+                        <i className="pi pi-key" />
+                        <Password id='password' className={`${passwordIsEmpty ? 'p-invalid' : ''}`} value={password} onChange={(e) => setPassword(e.target.value)} toggleMask  feedback={true} />
+                        <label htmlFor="password">Password*</label>
+                    </span>
+                </div>
+                <div className="field md-5">
+                    <span className="p-float-label p-input-icon-left mb-2">
+                        <i className="pi pi-key" />
+                        <Password id='passwordAgain' className={`${passwordAgainIsEmpty ? 'p-invalid' : ''}`} value={passwordAgain} onChange={(e) => setPasswordAgain(e.target.value)} toggleMask  feedback={false} />
+                        <label htmlFor="passwordAgain">Password Again*</label>
+                    </span>
+                </div>
+                </div>
+                <Button label='Register' onClick={() => {register()}} loading={isRegisterLoading} />
+                <p style={{textAlign: 'center'}}>Or if you already registered <Button label="login here" className="p-button-link inline p-0" />.</p>
+            </Card>
+    </div>
+    )
+}
+
+export default Register;
