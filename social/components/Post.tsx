764fb671f924fcc790c58e7afd144dd740c0ec1c
Botond Miller <gepsonka@pop-os.localdomain>
2022-10-19 00:23:18 +0200

    problems with reducer and context

--- social/components/Post.tsx
+++ social/components/Post.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Context, createContext, useReducer } from "react";
 import { useRouter } from "next/router";
 import { Panel } from 'primereact/panel';
 import { Avatar } from 'primereact/avatar';
@@ -9,10 +9,94 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
 import 'primereact/resources/primereact.min.css';
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
+import { Card } from "primereact/card";
 
+export interface PostComment {
+    userId: number,
+    avatarUrl: string,
+    content: string,
+    createdAt: Date
+}
+
+export interface PostReducerState {
+    comments: PostComment[],
+    createCommentContent: string,
+}
+
+
+const initialState: PostReducerState = {
+    comments: [],
+    createCommentContent: ''
+}
+
+enum PostActionsKind {
+    UPDATE_NEW_COMMENT = 'UPDATE_NEW_COMMENT',
+    CREATE_NEW_COMMENT_BLOCK = 'CREATE_NEW_COMMENT_BLOCK'
+}
+
+interface PostAction {
+    type: PostActionsKind,
+    payload: string |  PostComment
+}
+
+
+const reducer = (state: PostReducerState, action: PostAction) => {
+    const {type, payload} = action;
+    switch (type){
+        case PostActionsKind.UPDATE_NEW_COMMENT:
+            return {
+                comments: [
+                    ...state.comments
+                ],
+                createCommentContent: payload
+            }
+
+        case PostActionsKind.CREATE_NEW_COMMENT_BLOCK:
+            return {
+                comments: [
+                    ...state.comments,
+                    payload
+                ],
+                createCommentContent: state.createCommentContent
+            }
+    }
+}
+
+
+const PostContext = createContext<PostReducerState | null>(null);
 
 const Post = (props: PostProps) => {
     const router = useRouter();
+    const [state, dispatch] = useReducer(reducer, {
+        comments: [],
+        createCommentContent: ''
+    });
+
+    const providerValue = {
+        commentsBlocks: state!.comments,
+        addCommentBlock:  (commentBlock: PostComment) => {
+            dispatch({type: PostActionsKind.CREATE_NEW_COMMENT_BLOCK, commentBlock});
+        },
+
+        updateComment: (commentContent: string) => {
+            dispatch({type: PostActionsKind.UPDATE_NEW_COMMENT, commentContent});
+        }
+    }
+
+    const responsiveOptions = [
+        {
+            breakpoint: '1024px',
+            numVisible: 5
+        },
+        {
+            breakpoint: '768px',
+            numVisible: 3
+        },
+        {
+            breakpoint: '560px',
+            numVisible: 1
+        }
+    ];
 
     const headerTemplate = () => {
 
@@ -26,16 +110,28 @@ const Post = (props: PostProps) => {
         )
     }
 
+
+    const footerTemplate = () => {
+
+        return (
+            <div></div>
+        )
+    }
+
+    
+
     return (
-        <Panel headerTemplate={headerTemplate}>
-            <div>
-                <h2>Title</h2>
-                <p>DKASMLDKMSLM</p>
-            </div>
-            <div>
-                <Button icon="pi pi-heart" className="p-button-danger p-button-rounded" />
-            </div>
-        </Panel>
+        <PostContext.Provider value={providerValue}>
+            <Card header={headerTemplate}>
+                <div>
+                    <h2>Title</h2>
+                    <p>DKASMLDKMSLM</p>
+                </div>
+                <div>
+                    <Button icon="pi pi-heart" className="p-button-danger p-button-rounded" />
+                </div>
+            </Card>
+        </PostContext.Provider>
     )
 }
 
